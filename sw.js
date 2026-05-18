3d65c225749e408a652b854b9ff4c0f81131e431
civil-mac <legerem16@gmail.com>
2026-05-18 12:37:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+const CACHE_NAME = 'ari-shuffler-v1';
 
+// Wakes up the service worker
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+// Intercepts fetch requests so Chrome recognizes it as a true app background process
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
